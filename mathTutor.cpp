38962c4533fe4176caf17e707a0c603de9372c1a
Keshden <bleonrey7@gmail.com>
2019-04-30 11:44:00 -0600

    Added how I would accomplish version 2.0

--- mathTutor.cpp
+++ mathTutor.cpp
@@ -8,7 +8,10 @@
   Version 1.0: This will be a simple program that will iterate once, for now, and display corresponding message for correct or incorrect submissions.
   Version 2.0: This will add a lot of functionality, such as:
     -It will provide as many 'problems' as desired by user, using a do while loop
-    (?)-It will not only provide the 'incorrect' message, it will also provide a walk through to help them learn faster.
+ (?)-It will not only provide the 'incorrect' message, it will also provide a walk through to help them learn faster.
+        -Use the %10 in order to get the 1's positions.
+        -Use the /10, /100 to get how many 10's and hundreds there are and then *10 or *100 to revert without the 1's/10's present
+        -Using all of these methods, can go through the process step by step to show how to do addition of big numbers
 */
 
 #include<iostream>
