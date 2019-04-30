205d2d57d5bc27b461b8122c3fb0c744c822b6a9
Keshden <bleonrey7@gmail.com>
2019-04-30 11:38:21 -0600

    create the 2 files needed to start proceeding with the challenges I have chosen.

---
+++ bankCharges.cpp
@@ -0,0 +1,28 @@
+/*
+  bankCharges.cpp created by Keshden 4/30/2019
+  A bank charges $10 per month plus the following check fees for a commercial checking account.
+  $.10 each for fewer than 20 checks
+  $.08 each for 20-39 checks
+  $.06 each for 40-59 checks
+  $.04 each for 60 or more checks
+
+  The bank also charges an extra $15 if the balance of the acct falls below $400
+  (before any check fees are applied). Write a program that asks for the beginning
+  balance and the # of checks written.  Compute and display bank's service fees
+  for the month.
+
+  Input Validation: Do not accept a negative value for # of checks written.  If
+  neg. value is given for the beginning balance, display an urgent msg. indicating
+  the acct is overdrawn.
+*/
+
+#include<iostream>
+#include<cstdlib>
+using namespace std;
+
+int main()
+{
+
+
+  return 0;
+}
---
+++ mathTutor.cpp
@@ -0,0 +1,23 @@
+/*
+  mathTutor.cpp created by Keshden 4/30/2019
+  This will simply display 2 random numbers and ask the user to solve.
+  if the user correctly inputs the answer, the program will display thusly.
+  IE: "Congratulations, that is correct".
+  If incorrect, a message will be displayed for that as well.
+
+  Version 1.0: This will be a simple program that will iterate once, for now, and display corresponding message for correct or incorrect submissions.
+  Version 2.0: This will add a lot of functionality, such as:
+    -It will provide as many 'problems' as desired by user, using a do while loop
+    (?)-It will not only provide the 'incorrect' message, it will also provide a walk through to help them learn faster.
+*/
+
+#include<iostream>
+#include<cstdlib>
+using namespace std;
+
+int main()
+{
+
+
+  return 0;
+}
