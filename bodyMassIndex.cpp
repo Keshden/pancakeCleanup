bf378d7ed70d7f37f7a42183dcb0bd19cdcdab9f
Keshden <bleonrey7@gmail.com>
2019-05-10 03:36:24 -0600

    Finished adding and validating desired functions for the bodyMassIndex program. That means it now has the following functions implemented: 1)Repeatability 2)Displays the optimal weight range for a person's recorded height 3)Validates height and weight values and forces user to use correct and plausible values 4)quits when user states so.

--- bodyMassIndex.cpp
+++ bodyMassIndex.cpp
@@ -7,6 +7,7 @@
   Updated to have above 2 mentioned functionality by Keshden as of 5/8/2019
     -After calculations ask user if they wish to repeat
     -Give user option to see what 'weight ranges' would be optimal
+  Updated again to finish off the desired functionality. By: Keshden as of 5/10/2019
 */
 
 #include<iostream>
@@ -16,14 +17,15 @@ using namespace std;
 
 int main()
 {
-  int weight, height;
-  float bmi;
+  int weight, height, userChoice;
+  float bmi, lowerRange, upperRange;
   bool correct = false, repeat = false;
+  char userConfirmation = 'N';
 
   cout << "Welcome to the Body Mass Index calculator.\n\n"
     << "This program will ask you for your weight and height and calculate your BMI." << endl;
 
-  cout << "The program will also let you know if the BMI indicates underweight,\n overweight, or optimal.\n\n";
+  cout << "The program will also let you know if the BMI indicates underweight,\noverweight, or optimal.\n\n";
   do
   {
     do
@@ -41,7 +43,7 @@ int main()
         }
         else
         {
-          cout << "You have entered in a weight greater than 1500.";
+          cout << "You have entered in a weight greater than 1500.\n";
           cout << "According to the Guinness Book of \nWorld Records, the heaviest person recorded ";
           cout << "weighed 1235 Lbs.\nPlease verify that you have correctly entered in your weight.\n";
           cout << "Please enter your weight in pounds.\n";
@@ -71,7 +73,6 @@ int main()
         }
       }
 
-      char userConfirmation = 'N';
       cout << "You have entered your weight as: " << weight << ".\nYou have also entered"
       << "your height as: " << height << ".\nIs this information correct?[Y/N].\n";
       cin >> userConfirmation;
@@ -94,9 +95,47 @@ int main()
       cout << "Your BMI is within the optimal range for your weight and height.\n\n";
     else
       cout << "Your BMI is below the optimal range for your weight and height.\n\n";
-  //-After calculations ask user if they wish to repeat(2nd)
-  //-Give user option to see what 'weight ranges' would be optimal(1st)
 
+    userChoice = 0;
+    while(userChoice < 1 || userChoice > 3)
+    {
+      cout << "\n\nThanks for utilizing the Body Mass Index program.";
+      cout << "\n\nPlease choose among the following options:";
+      cout << "\n1)View your optimal weight range.";
+      cout << "\n2)Repeat the program from the beginning.";
+      cout << "\n3)Quit or End program.\n";
+      cout << "Please enter either a 1, a 2, or a 3.\n";
+      cin >> userChoice;
+    }
+
+    switch (userChoice)
+    {
+      //if bmi needs to be between 18.5 and 25 then:
+      //to calculate the weight range would be using 18.5 and 25 for x
+      //where (x(height times height))/703 = weight
+      case 1:
+
+              lowerRange = static_cast<double>(18.5*(height*height))/703;
+              upperRange = static_cast<double>(25*(height*height))/703;
+              cout << "The optimal weight range will be calculated based on the height you provided.";
+              cout << "\nProvided height is: " << height;
+              cout << "\nThe optimal weight range is: Between " << lowerRange
+                << " and " << upperRange << endl;
+              cout << "Repeat program? [Y/N]: \n";
+              cin >> userConfirmation;
+              if(userConfirmation == 'y' || userConfirmation == 'Y')
+                repeat = true;
+              else
+                repeat = false;
+              break;
+      case 2:
+              cout << "\n\n";
+              repeat = true;
+              break;
+      case 3:
+              repeat = false;
+              break;
+    }
   } while(repeat == true);
 
   return 0;
